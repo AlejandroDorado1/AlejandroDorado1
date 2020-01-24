9bc39dbb89f83486d9315991ba04313e917cf131
AlejandroDorado1 <55292283+AlejandroDorado1@users.noreply.github.com>
2020-01-24 13:13:57 -0500

    Update myScripts.js

--- JS/myScripts.js
+++ JS/myScripts.js
@@ -1,8 +1,18 @@
-function myLoop(){
-var start = document.getElementById("start").value;
+function myLoop(){ 
+var start = document.getElementById("start").value;  
 var end = document.getElementById("end").value;
-for(var i = start; i <= end; i++)
+var jump = document.getElementById("jump").value;
+
+console.log(typeof start);
+console.log(typeof end);
+console.log(typeof jump);
+  
+start = Number(start);
+end = Number(end);
+jump = Number(jump);  
+  
+for(var i = start; i <= end; i+=jump)
   {
-    document.write('<p>'+i+'</p>')
+    document.write('<p>' + i + '</p>');
+  }  
   }
-}
